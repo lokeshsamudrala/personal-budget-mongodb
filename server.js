ba347f0d8563b3e5994c4bde7318b7b406bf30af
Lokesh Samudrala <lokeshsamudrala@Lokeshs-MacBook-Air.local>
2023-09-16 18:37:48 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -4,22 +4,7 @@ const port = 3000;
 
 app.use('/',express.static('public'));
 
-const budget = {
-    myBudget: [
-    {
-        title:'Eat out',
-        budget: 20
-    },
-    {
-        title: 'Rent',
-        budget: 350
-    },
-    {
-        title: 'Groceries',
-        budget: 90
-    },
-    ]
-};
+const budget = require('./budget.json');
 
 app.get('/hello', (req,res) => {
     res.send('Hello World!');
